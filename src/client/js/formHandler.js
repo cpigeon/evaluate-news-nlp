edffe2ab8c1bf99beda2f153b26fad30b677a1d1
ChristaPigeon <cstath26@gmail.com>
2021-04-20 16:20:00 -0700

    feat: Implement updateUI
    Implement updateUI, which updates the page based off the information
    received from the MeaningCloud API on the server side

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -46,10 +46,20 @@ function handleSubmit(event) {
             body: JSON.stringify({formURL}),
         })
         .then((res) => res.json())
-        // .then((res) => {
-        //     updateUI(res);
-        // })
+        .then((res) => {
+            updateUI(res);
+        })
+}
 
+const updateUI = async (res) => {
+  console.log(res);
+  document.getElementById('results').insertAdjacentHTML('beforeend', "Website: " + res.sentimented_entity_list[0].form);
+  document.getElementById('results').insertAdjacentHTML('beforeend', "<br> Polarity: " + res.score_tag.toLowerCase());
+  document.getElementById('results').insertAdjacentHTML('beforeend', "<br> Agreement: " + res.agreement.toLowerCase());
+  document.getElementById('results').insertAdjacentHTML('beforeend', "<br> Subjectivity: " + res.subjectivity.toLowerCase());
+  document.getElementById('results').insertAdjacentHTML('beforeend', "<br> Confidence: " + res.confidence.toLowerCase());
+  document.getElementById('results').insertAdjacentHTML('beforeend', "<br> Irony: " + res.irony.toLowerCase());
+  document.getElementById('results').insertAdjacentHTML('beforeend', "<br><br>");
 }
 
 export { handleSubmit }
