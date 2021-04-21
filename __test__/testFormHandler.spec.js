46c8df02a1d06447167cfb9a8c991c20fbf0eaef
ChristaPigeon <cstath26@gmail.com>
2021-04-21 12:23:55 -0700

    feat: Write Jest test for handleSubmit function

--- __test__/testFormHandler.spec.js
+++ __test__/testFormHandler.spec.js
@@ -1,1 +1,14 @@
 import { handleSubmit } from "../src/client/js/formHandler"
+
+// describe takes two arguments...
+// 1. a string description of the test
+// 2. callback function for wrapping the actual test
+describe("Testing the submit functionality", () => {
+  // test takes two arguments as well...
+  // 1. a string description
+  // 2. actual test as a callback function
+  test("Testing the handleSubmit() function", () => {
+    // actual test
+    expect(handleSubmit).toBeDefined();
+  });
+});
